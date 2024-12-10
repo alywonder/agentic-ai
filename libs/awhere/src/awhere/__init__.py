9bd75e3a06ccb5e65d7fa32f7730e2dc3cda5f30
Al Wonder <al@yiwonder.com>
2024-12-09 19:56:31 -0800

    clean awhere package base

--- libs/awhere/src/awhere/__init__.py
+++ libs/awhere/src/awhere/__init__.py
@@ -1,3 +1,4 @@
-def hello() -> str:
-    """Return greeting."""
-    return 'Hello from awhere!'
+# Copyright 2024 Weavers @ Eternal Loom. All rights reserved.
+#
+# Use of this software is governed by the license that can be
+# found in LICENSE file in the source repository.
